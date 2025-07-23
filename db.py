3c4b8c57e51e57b69b9b9309784d3abbf3abc99a
Samandar <samandarsiddiqov31@gmail.com>
2025-07-23 10:10:15 +0500

    Databaza to'liq shakllandi

--- db.py
+++ db.py
@@ -1,5 +1,26 @@
 import sqlite3
 from config import DB_NAME
 
+def create_tables():
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                telegram_id INTEGER UNIQUE,
+                full_name TEXT,
+                phone_number TEXT
+            );
+        """)
+        conn.commit()
+
+def add_user(telegram_id, full_name, phone_number):
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT OR REPLACE INTO users (telegram_id, full_name, phone_number)
+            VALUES (?, ?, ?)
+        """, (telegram_id, full_name, phone_number))
+        conn.commit()
 
 
